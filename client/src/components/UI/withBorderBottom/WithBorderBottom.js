456cb18224e31a29ba21b9b4c3f03f127dae8285
JimenaMartin <jimena.martin@cloudappi.net>
2018-08-06 17:44:02 +0200

    package json

--- client/src/components/UI/withBorderBottom/WithBorderBottom.js
+++ client/src/components/UI/withBorderBottom/WithBorderBottom.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Border = styled.div`
 width: ${props => props.width ? props.width : '20%'};
 height: ${props => props.height ? props.height : '170px'};
-margin-bottom: ${props => props.bottom ? `${props.bottom}px` : '0'};
+margin-bottom: ${props => props.bottom ? `${props.bottom}px` : '3px'};
 position:relative;
  :hover {
    border-bottom: ${props => props.color ? `3px solid ${props.color}` : '3px solid #2b9b41'};
